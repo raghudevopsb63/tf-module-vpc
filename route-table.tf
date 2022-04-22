197201d5d3985efe93e43f1206ba0d72ec360586
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-22 07:27:23 +0530

    Update terraform/tf-module-vpc/

--- route-table.tf
+++ route-table.tf
@@ -11,3 +11,9 @@ resource "aws_route_table" "route-table" {
   }
 }
 
+
+resource "aws_route_table_association" "rt-assoc" {
+  count          = length(aws_subnet.main.*.id)
+  subnet_id      = element(aws_subnet.main.*.id, count.index)
+  route_table_id = aws_route_table.route-table.id
+}
