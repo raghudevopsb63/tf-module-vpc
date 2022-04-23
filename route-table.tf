7abfdbacaa6514bf0ec13b6f163a45c5a6ddb6db
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-23 07:23:07 +0530

    Update terraform/tf-module-vpc/

--- route-table.tf
+++ route-table.tf
@@ -49,9 +49,9 @@ resource "aws_route_table_association" "private-rt-assoc" {
   route_table_id = aws_route_table.private-route-table.id
 }
 
-//resource "aws_route" "route-to-default-vpc-rt" {
-//  route_table_id            = var.DEFAULT_VPC_RT
-//  destination_cidr_block    = var.VPC_CIDR
-//  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
-//}
-//
+resource "aws_route" "route-to-default-vpc-rt" {
+  route_table_id            = var.DEFAULT_VPC_RT
+  destination_cidr_block    = var.VPC_CIDR
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+}
+
