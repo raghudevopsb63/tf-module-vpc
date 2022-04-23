0b1ac070edbe48157d6bb2609eafe576f6b35f81
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-23 07:00:43 +0530

    Update terraform/tf-module-vpc/

--- route-table.tf
+++ route-table.tf
@@ -1,23 +1,28 @@
-//resource "aws_route_table" "route-table" {
-//  vpc_id = aws_vpc.main.id
-//
-//  route {
-//    cidr_block                = var.DEFAULT_VPC_CIDR
-//    vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
-//  }
-//
-//  tags = {
-//    Name = "${var.ENV}-route-table"
-//  }
-//}
-//
-//
-//resource "aws_route_table_association" "rt-assoc" {
-//  count          = length(aws_subnet.main.*.id)
-//  subnet_id      = element(aws_subnet.main.*.id, count.index)
-//  route_table_id = aws_route_table.route-table.id
-//}
-//
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.ENV}-pub-route-table"
+  }
+}
+
+
+resource "aws_route_table_association" "public-rt-assoc" {
+  count          = length(aws_subnet.public.*.id)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public-route-table.id
+}
+
 //resource "aws_route" "route-to-default-vpc-rt" {
 //  route_table_id            = var.DEFAULT_VPC_RT
 //  destination_cidr_block    = var.VPC_CIDR
