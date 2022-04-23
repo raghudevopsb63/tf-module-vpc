35193f26215ced002a8f6d3eda5d3244a4daeaa2
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-23 07:11:44 +0530

    Update terraform/tf-module-vpc/

--- route-table.tf
+++ route-table.tf
@@ -23,6 +23,31 @@ resource "aws_route_table_association" "public-rt-assoc" {
   route_table_id = aws_route_table.public-route-table.id
 }
 
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer-connection.id
+  }
+
+  route {
+    cidr_block      = "0.0.0.0/0"
+    aws_nat_gateway = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = "${var.ENV}-priv-route-table"
+  }
+}
+
+
+resource "aws_route_table_association" "private-rt-assoc" {
+  count          = length(aws_subnet.private.*.id)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private-route-table.id
+}
+
 //resource "aws_route" "route-to-default-vpc-rt" {
 //  route_table_id            = var.DEFAULT_VPC_RT
 //  destination_cidr_block    = var.VPC_CIDR
